77731e92ab67e96e4a09c52212a797f65af90d47
kbsmith01 <kbsmith01@TS15849.ysu.local>
2018-02-21 12:48:51 -0500

    Class over.

--- src/java_class_prg_00_07_exception_handling/JAVA_CLASS_PRG_00_07_Exception_Handling.java
+++ src/java_class_prg_00_07_exception_handling/JAVA_CLASS_PRG_00_07_Exception_Handling.java
@@ -5,6 +5,8 @@
  */
 package java_class_prg_00_07_exception_handling;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
 /**
  *
  * @author kbsmith01
@@ -15,7 +17,51 @@ public class JAVA_CLASS_PRG_00_07_Exception_Handling {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        boolean looping = true;
+        
+        int numerator = 1;
+        int denominator = 1;
+        int result = 1;
+        
+        Scanner keyboard = new Scanner(System.in);
+        
+        
+
+        
+        do {
+            try {
+                System.out.print("Enter an integer numerator: ");
+                numerator = keyboard.nextInt();
+                looping = false;
+            }
+            catch(InputMismatchException ime) {
+                keyboard.nextLine();
+                System.out.println("\nThat is not an integer value.");
+            }
+        } while(looping);
+        
+        looping = true;
+        do {
+            try {
+                System.out.print("Enter an integer denominator: ");
+                denominator = keyboard.nextInt();
+                looping = false;
+            }
+            catch(InputMismatchException imeEx) {
+                System.out.println("\nThat is not an integer value.");
+            }
+        } while(looping);
+        
+        try {
+            result = numerator / denominator;
+            System.out.println("The result it " + result);
+        }
+        catch(ArithmeticException aeEx) {
+            System.out.println("Arithmetic error");
+        }
+        
+        
+        System.out.println("Goodbye");
     }
     
 }
